149a0a855918b2c4612f18fe2a89774aace92367
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-01-28 13:42:27 -0300

    Add SerializationTest

---
+++ src/test/java/com/flowingcode/vaadin/addons/xterm/test/SerializationTest.java
@@ -0,0 +1,37 @@
+package com.flowingcode.vaadin.addons.xterm.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+import com.flowingcode.vaadin.addons.xterm.XTerm;
+import com.flowingcode.vaadin.addons.xterm.XTermClipboard;
+import com.flowingcode.vaadin.addons.xterm.XTermConsole;
+import com.flowingcode.vaadin.addons.xterm.XTermFit;
+
+public class SerializationTest {
+	
+	private void testSerializationOf(Object obj) throws IOException, ClassNotFoundException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+			oos.writeObject(new XTerm());
+		}
+		
+		try (ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
+			XTerm.class.cast(in.readObject());
+		}
+	}
+
+	@Test
+	public void testSerialization() throws ClassNotFoundException, IOException {
+		testSerializationOf(new XTerm());
+		testSerializationOf(new XTermClipboard());
+		testSerializationOf(new XTermConsole());
+		testSerializationOf(new XTermFit());
+	}
+}
