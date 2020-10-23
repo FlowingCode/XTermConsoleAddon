1e16528231658d8cf4bfcbdcbec192d8b72f7bf8
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-23 10:35:19 -0300

    test(demo): add demo layout

--- src/test/java/com/flowingcode/vaadin/addons/xterm/test/LayoutTest.java
+++ src/test/java/com/flowingcode/vaadin/addons/xterm/test/LayoutTest.java
@@ -17,22 +17,24 @@
  * limitations under the License.
  * #L%
  */
+package com.flowingcode.vaadin.addons.xterm.test;
 
-package com.flowingcode.vaadin.addons.xterm;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
+import org.junit.Test;
+
+import com.flowingcode.vaadin.addons.DemoLayout;
+import com.flowingcode.vaadin.addons.xterm.XtermDemoView;
 import com.vaadin.flow.router.Route;
 
-@SuppressWarnings("serial")
-@Route("")
-public class DemoView extends VerticalLayout implements BeforeEnterObserver {
+public class LayoutTest {
 
-	@Override
-	public void beforeEnter(BeforeEnterEvent event) {
-		event.forwardTo(XtermDemoView.class);
+	@Test
+	public void testDemoLayout() {
+		Route route = XtermDemoView.class.getAnnotation(Route.class);
+		assertEquals("com.flowingcode.vaadin.addons.DemoLayout",DemoLayout.class.getName());
+		assertEquals(DemoLayout.class, route.layout());
+		assertNotEquals("", route.value());
 	}
-
 }
-
