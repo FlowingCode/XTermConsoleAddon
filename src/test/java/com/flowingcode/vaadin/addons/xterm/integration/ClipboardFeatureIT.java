c796bfb7ea5d8fb266f758d8da7db7f0face426d
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-04-21 08:47:26 -0300

    style: apply code formatting

--- src/test/java/com/flowingcode/vaadin/addons/xterm/integration/ClipboardFeatureIT.java
+++ src/test/java/com/flowingcode/vaadin/addons/xterm/integration/ClipboardFeatureIT.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,33 +22,36 @@ package com.flowingcode.vaadin.addons.xterm.integration;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import com.flowingcode.vaadin.addons.xterm.XTermClipboard;
+import com.vaadin.testbench.TestBenchElement;
 import org.junit.Test;
 import org.openqa.selenium.interactions.Actions;
 
-import com.flowingcode.vaadin.addons.xterm.XTermClipboard;
-import com.vaadin.testbench.TestBenchElement;
-
 public class ClipboardFeatureIT extends AbstractXTermTest {
-	
-	@Test 
-	public void testFeature() {
-		TestBenchElement term = $("fc-xterm").first();
-		assertFeatureHasBeenLoaded(term, XTermClipboard.class, "clipboard");
-		
-		write(term, "\\x1bcTEXT");
-		
-		int[] size = intArray(getCommandExecutor().executeScript("return [arguments[0].clientWidth, arguments[0].clientHeight]", term));
-		getCommandExecutor().executeScript("arguments[0].useSystemClipboard='false'", $("fc-xterm-clipboard").first());
-		
-		new Actions(driver)
-			.moveToElement(term, -size[0]/2, -size[1]/2)
-			.clickAndHold()
-			.moveByOffset(100,0)
-			.release()
-			.perform();
-		
-		new Actions(driver).contextClick().perform();
-		assertThat(currentLine(term), is("TEXTTEXT"));
-	}
 
+  @Test
+  public void testFeature() {
+    TestBenchElement term = $("fc-xterm").first();
+    assertFeatureHasBeenLoaded(term, XTermClipboard.class, "clipboard");
+
+    write(term, "\\x1bcTEXT");
+
+    int[] size =
+        intArray(
+            getCommandExecutor()
+                .executeScript(
+                    "return [arguments[0].clientWidth, arguments[0].clientHeight]", term));
+    getCommandExecutor()
+        .executeScript("arguments[0].useSystemClipboard='false'", $("fc-xterm-clipboard").first());
+
+    new Actions(driver)
+        .moveToElement(term, -size[0] / 2, -size[1] / 2)
+        .clickAndHold()
+        .moveByOffset(100, 0)
+        .release()
+        .perform();
+
+    new Actions(driver).contextClick().perform();
+    assertThat(currentLine(term), is("TEXTTEXT"));
+  }
 }
