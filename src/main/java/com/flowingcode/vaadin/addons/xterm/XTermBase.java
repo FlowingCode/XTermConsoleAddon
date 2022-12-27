a06db1590282b3c3314ff39d47c72152294ca877
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-12-27 15:31:40 -0300

    build: upgrade to XTerm version 5.1.0

--- src/main/java/com/flowingcode/vaadin/addons/xterm/XTermBase.java
+++ src/main/java/com/flowingcode/vaadin/addons/xterm/XTermBase.java
@@ -57,7 +57,7 @@ import lombok.experimental.Delegate;
 
 /** Server-side component for the XTerm component. */
 @SuppressWarnings("serial")
-@NpmPackage(value = "xterm", version = "4.19.0")
+@NpmPackage(value = "xterm", version = "5.1.0")
 @JsModule("./fc-xterm/xterm-element.ts")
 @CssImport("xterm/css/xterm.css")
 public abstract class XTermBase extends Component
