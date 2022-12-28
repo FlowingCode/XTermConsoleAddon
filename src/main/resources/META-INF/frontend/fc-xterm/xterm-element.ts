98a0f2ae7e6ef423a1cbb63db3b4b0fa860e9455
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-12-28 16:42:46 -0300

    refactor: add bellSound and bellStyle as properties of fc-xterm
    Close #73

--- src/main/resources/META-INF/frontend/fc-xterm/xterm-element.ts
+++ src/main/resources/META-INF/frontend/fc-xterm/xterm-element.ts
@@ -136,7 +136,10 @@ export class XTermElement extends LitElement implements TerminalMixin {
   terminal: Terminal;
   disabled: boolean = false;
   node: XTermElement;
-
+  
+  bellSound: string;
+  bellStyle: 'none' | 'sound'
+  
   customKeyEventHandlers: CustomKeyEventHandlerRegistry;
 
   render(): TemplateResult {
@@ -150,6 +153,12 @@ export class XTermElement extends LitElement implements TerminalMixin {
     this.customKeyEventHandlers = new CustomKeyEventHandlerRegistry();
     this.terminal = new Terminal();	
 	this.node = this;
+    
+    //https://gist.github.com/literallylara/7ece1983fab47365108c47119afb51c7
+    //(C) Lara Sophie Schütt 2016, CC0 
+    for(var i=44100*0.1,d="";i--;)d+=String.fromCharCode(~~((Math.sin(i/44100*2*Math.PI*800)+1)*128)); 
+    this.bellSound = "data:Audio/WAV;base64,"+btoa("RIFFdataWAVEfmt "+atob("EAAAAAEAAQBErAAARKwAAAEACABkYXRh/////w==")+d);
+    this.bellStyle = 'none';
   }
 
   connectedCallback() {
@@ -163,11 +172,12 @@ export class XTermElement extends LitElement implements TerminalMixin {
     term.onData(e => {
         term.write(e.replace(/\r/g,'\x1b[<N\n'));
     });
-            
-    //https://gist.github.com/literallylara/7ece1983fab47365108c47119afb51c7
-    //(C) Lara Sophie Schütt 2016, CC0 
-    for(var i=44100*0.1,d="";i--;)d+=String.fromCharCode(~~((Math.sin(i/44100*2*Math.PI*800)+1)*128)); 
-    term.options.bellSound = "data:Audio/WAV;base64,"+btoa("RIFFdataWAVEfmt "+atob("EAAAAAEAAQBErAAARKwAAAEACABkYXRh/////w==")+d);
+    
+    term.onBell(() => {
+      if (this.bellStyle == 'sound') {
+        new Audio(this.bellSound).play();
+      }
+    });
 
     term.attachCustomKeyEventHandler(ev => {
       if (ev.type!=='keydown') return false;
